7c69d36912d3a31f472cf7dab32d37762324cd55
Buyanov Danila <info@saity74.ru>
2013-09-23 14:01:51 +0600

    blank_j3 template upload

---
+++ html/modules.php
@@ -0,0 +1,36 @@
+<?php
+    function modChrome_mainmenu($module, $params, $attribs)
+    {
+    
+        $wrapperClass = htmlspecialchars($params->get('moduleclass_sfx'));
+    
+        $html = '';
+    
+        if (!empty ($module->content)) {
+    
+            $html .= '<div class="navbar '.$wrapperClass.'">';
+            $html .= '<div class="navbar-inner">';
+            $html .= '<div class="container">';
+            
+            $html .= $module->content;
+            
+            $html .= '</div>';
+            $html .= '</div>';
+            $html .= '</div>';
+    
+        }
+    
+        echo $html;
+    }
+    
+    function modChrome_default($module, &$params, &$attribs)
+    {
+            if ($module->content){
+                    echo "<div class=\"module ". htmlspecialchars($params->get('moduleclass_sfx')) ."\">";
+                    if ($module->showtitle)
+                        echo "<h3 class=\"module-header\">".$module->title."</h3>";
+                    echo $module->content;
+                    echo "</div>";
+            }
+    }
+?>
